be0860d3c822894a2986f164fa10919b763503b1
Josh Cook <joshuacook0023@gmail.com>
2021-02-27 10:09:24 -0500

    feat: add metadata to a tissue slide object

--- R/tissue_slide.R
+++ R/tissue_slide.R
@@ -3,22 +3,24 @@
 #' An S3 class to hold the information for an individual slide.
 #'
 #' @param slide_data The data from the slide.
+#' @param metadata Additional metadata as a list with arbitrary structure and values.
 #'
 #' @return A \code{tissue_slide} object that is a subclass of a tibble.
 #'
 #' @examples
 #' pancreas_slide <- tissue_slide(pancreas)
 #' @export tissue_slide
-tissue_slide <- function(slide_data) {
-  new_tissue_slide(slide_data)
+tissue_slide <- function(slide_data, metadata = list()) {
+  new_tissue_slide(slide_data, metadata = metadata)
 }
 
 
-new_tissue_slide <- function(data = tibble::tibble()) {
+new_tissue_slide <- function(data = tibble::tibble(), metadata = list()) {
   stopifnot(inherits(data, "data.frame"))
   structure(
     data,
     manual_class = manual_classification(),
+    metadata = metadata,
     class = c("tissue_slide", class(tibble::tibble()))
   )
 }
@@ -27,8 +29,13 @@ new_tissue_slide <- function(data = tibble::tibble()) {
 validate_tissue_slide <- function(ts) {
   stopifnot(inherits(ts, "tissue_slide"))
   stopifnot(inherits(ts, "data.frame"))
+
   assertr::verify(ts, assertr::has_all_names("x", "y"))
 
+  stopifnot("metadata" %in% names(attributes(ts)))
+  stopifnot(is.list(attr(ts, "metadata")))
+
+  stopifnot("manual_class" %in% names(attributes(ts)))
   stopifnot(inherits(attr(ts, "manual_class"), "manual_classification"))
 
   return(TRUE)
@@ -64,5 +71,44 @@ set_manual_classification <- function(ts, manual_class) {
   validate_manual_classification(manual_class)
   attr(ts, "manual_class") <- manual_class
   validate_tissue_slide(ts)
+  return(ts)
+}
+
+
+#' Get metadata from a tissue slide.
+#'
+#' Extract the metadata stored with a tissue slide object. To set the metadata, use the function \code{set_slide_metadata()}.
+#'
+#' @param ts A Tissue slide object.
+#'
+#' @return The metadata object associated with the slide.
+#'
+#' @examples
+#' slide <- tissue_slide(pancreas, metadata = list(tissue = "pancreas", mouse = "OP24"))
+#' get_slide_metadata(slide)
+#' @export get_slide_metadata
+get_slide_metadata <- function(ts) {
+  validate_tissue_slide(ts)
+  return(attr(ts, "metadata"))
+}
+
+
+#' Set metadata from a tissue slide.
+#'
+#' Write the metadata stored with a tissue slide object. To get the metadata, use the function \code{get_slide_metadata()}.
+#'
+#' @param ts A Tissue slide object.
+#' @param metadata Metadata of arbitrary structure.
+#'
+#' @return Returns the tissue slide object with the metadata.
+#'
+#' @examples
+#' slide <- tissue_slide(pancreas, metadata = list(tissue = "pancreas", mouse = "OP24"))
+#' slide <- set_slide_metadata(slide, list(tissue = "not pancreas", mouse = "Mickey"))
+#' get_slide_metadata(slide)
+#' @export set_slide_metadata
+set_slide_metadata <- function(ts, metadata) {
+  validate_tissue_slide(ts)
+  attr(ts, "metadata") <- metadata
   return(ts)
 }
