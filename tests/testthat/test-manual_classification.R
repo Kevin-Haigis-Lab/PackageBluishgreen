3e510e9d5a829b5a3ec3a5727a11b58e196da5ea
Josh Cook <joshuacook0023@gmail.com>
2021-03-03 09:48:26 -0500

    test: manual classification getter functions

--- tests/testthat/test-manual_classification.R
+++ tests/testthat/test-manual_classification.R
@@ -19,3 +19,25 @@ test_that("can create a manual classification with numbers", {
   expect_true(attr(mc, "cutoff") == 3)
   expect_true(all(mc == vals))
 })
+
+test_that("the manual classification cutoff getter works", {
+  mc <- manual_classification(1:5)
+  expect_true(is.na(get_manual_classification_cutoff(mc)))
+
+  cutoff <- 3
+  mc <- manual_classification(1:5, cutoff = cutoff)
+  expect_equal(get_manual_classification_cutoff(mc), 3)
+})
+
+test_that("the manual classification transform getter works", {
+  mc <- manual_classification(1:5)
+  expect_equal(get_manual_classification_transform(mc), identity)
+  mc <- manual_classification(1:5, transform = log10)
+  expect_equal(get_manual_classification_transform(mc), log10)
+
+  rando_function <- function(x) {
+    x
+  }
+  mc <- manual_classification(1:5, transform = rando_function)
+  expect_equal(get_manual_classification_transform(mc), rando_function)
+})
