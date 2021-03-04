93b12818991bb295c7e679097e5d07476f860e54
Josh Cook <joshuacook0023@gmail.com>
2021-03-04 11:30:52 -0500

    feat: parameterize plot_slide_clusters()

--- R/plot_slide_clusters.R
+++ R/plot_slide_clusters.R
@@ -16,8 +16,15 @@
 #' plot_slide_clusters(pancreas_slide)
 #' @import ggplot2
 #' @export plot_slide_clusters
-plot_slide_clusters <- function(ts, method = "manual", prop = 0.25) {
+plot_slide_clusters <- function(ts,
+                                method = "manual",
+                                prop = 0.25,
+                                point_size = 0.5,
+                                alpha_values = c(0.2, 0.7)) {
   if (method == "manual") {
+    if (is.na(get_manual_classification_cutoff(get_manual_classification(ts)))) {
+      stop("Cannot plot clusters without first applying a manual classification to the tissue slide.")
+    }
     ts$.cluster <- get_manual_classification(ts)
   } else {
     stop(paste(
@@ -28,11 +35,12 @@ plot_slide_clusters <- function(ts, method = "manual", prop = 0.25) {
       sep = "\n"
     ))
   }
+
   ts <- dplyr::slice_sample(ts, prop = prop)
   p <- ggplot2::ggplot(ts, ggplot2::aes(color = .cluster, alpha = .cluster)) +
     ggplot2::geom_point(
       ggplot2::aes(color = .cluster, alpha = .cluster),
-      size = 0.5
+      size = point_size
     ) +
     ggplot2::scale_x_continuous(expand = ggplot2::expansion(mult = c(0, 0))) +
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, 0))) +
@@ -42,7 +50,7 @@ plot_slide_clusters <- function(ts, method = "manual", prop = 0.25) {
       direction = -1,
       guide = ggplot2::guide_legend(override.aes = list(size = 1, alpha = 1))
     ) +
-    ggplot2::scale_alpha_manual(values = c(0.2, 0.7), guide = FALSE) +
+    ggplot2::scale_alpha_manual(values = alpha_values, guide = FALSE) +
     theme_tissue_slide() +
     ggplot2::labs(color = "cluster")
 }
